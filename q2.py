e66e154267bd7a4b59da72ef6f89bc4646d67a4e
Sourav Bhowal <souravbhowal15@gmail.com>
2025-01-27 19:49:18 +0530

    first commit

---
+++ q2.py
@@ -0,0 +1,5 @@
+# Calculate sum of two numbers
+a = 10
+b = 20
+sum = a + b
+print("Sum of a and b is: ", sum)
---
+++ q3.py
@@ -0,0 +1,4 @@
+# Find square of a number
+
+a = int(input("Enter a number: "))
+print("Square of the number is: ", a**2)
